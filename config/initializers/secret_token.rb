a2051bc6fce9f471d2ca5c22f16617004bbc360a
Charles Whittaker <charles.whittaker@gmail.com>
2014-03-09 10:52:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backdoor::Application.config.secret_token = 'bfbd02c0dc965aa1726ece52717459bbf936371bca6ecbd04effa47bcb89bfec846fc7c6645925c0f3fb202285bfe55c6c275eef5704b95f6643a4923c80b4f2'
